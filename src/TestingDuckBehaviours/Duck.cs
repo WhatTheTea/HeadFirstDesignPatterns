0f633622a67ae2bec45ed8926dde4e1285f2961f
WhatTheTea <silchenko.whatthetea@gmail.com>
2024-02-09 23:38:34 +0200

    Add virtual methods to duck

--- src/TestingDuckBehaviours/Duck.cs
+++ src/TestingDuckBehaviours/Duck.cs
@@ -5,7 +5,7 @@ public abstract class Duck
     IFly flyBehaviour;
     IQuack quackBehaviour;
     public abstract void Display();
-    public void Fly(){ }
-    public void Quack() {}
-    public void Swim() {}
+    public virtual void Fly() => flyBehaviour.Fly();
+    public virtual void Quack() => quackBehaviour.Quack();
+    public virtual void Swim() => Console.WriteLine("*splash-splash*");
 }
