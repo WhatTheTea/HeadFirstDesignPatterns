0623cdf1136dd90eb54fa10b60114c3c6773f3e1
WhatTheTea <silchenko.whatthetea@gmail.com>
2024-02-10 00:32:52 +0200

    Extend duck with dynamic behaviour

--- src/TestingDuckBehaviours/Duck.cs
+++ src/TestingDuckBehaviours/Duck.cs
@@ -2,11 +2,20 @@
 
 public abstract class Duck
 {
-    protected IFly flyBehaviour;
-    protected IQuack quackBehaviour;
+    protected IFly flyBehaviour = new FlyNot();
+    protected IQuack quackBehaviour = new QuackMute();
     public abstract void Display();
-    public virtual void Fly() => flyBehaviour.Fly();
-    public virtual void Quack() => quackBehaviour.Quack();
+    public virtual void Fly() =>
+        flyBehaviour.Fly();
+    public virtual void Quack() => 
+        quackBehaviour.Quack();
     // TODO: metal ducks xD
-    public virtual void Swim() => Console.WriteLine("*splash-splash*");
+    public virtual void Swim() => 
+        Console.WriteLine("*splash-splash*");
+
+    public virtual void SetFlyBehaviour(IFly fly) =>
+        flyBehaviour = fly;
+
+    public virtual void SetQuackBehaviour(IQuack quack) =>
+        quackBehaviour = quack;
 }
