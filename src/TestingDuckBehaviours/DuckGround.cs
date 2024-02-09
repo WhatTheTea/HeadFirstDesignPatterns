e0d36f0b684f51649d95a895108ec38bcfb95f3d
WhatTheTea <silchenko.whatthetea@gmail.com>
2024-02-10 00:47:24 +0200

    Add grounded duck

---
+++ src/TestingDuckBehaviours/DuckGround.cs
@@ -0,0 +1,14 @@
+﻿namespace TestingDuckBehaviours;
+
+public class DuckGround : Duck
+{
+    public DuckGround()
+    {
+        quackBehaviour = new Quack();
+        flyBehaviour = new FlyNot();
+    }
+    public override void Display()
+    {
+        Console.WriteLine("Grounded duck :(");
+    }
+}
